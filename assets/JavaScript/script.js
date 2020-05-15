5e3b593486ec06661e106d6e2e5fa3d7e8e899af
Emily Renegar <egrenegar@gmail.com>
2020-05-15 15:06:11 -0400

    added radio buttons to search box and altered positioning of divs

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,1 +1,5 @@
-console.log()
+console.log("link");
+var queryString = window.location.search;
+var urlParams = new URLSearchParams(queryString);
+var keyword = urlParams.get("keyword");
+console.log(keyword);

6e88527f31e19df74054e014ce407eafa2b98d20
Helen Darden <hgdarden@gmail.com>
2020-05-15 14:40:32 -0400

    added js

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,6 +1,9 @@
 // ------------------ Page 1 ---------------------------
 
-
+var queryString = window.location.search;
+      var urlParams = new URLSearchParams(queryString);
+      var keyword = urlParams.get("keyword");
+      console.log(keyword);
 
 
 

b03fd0e9fb8efd9cbe806c488aff788f1f34a664
Helen Darden <hgdarden@gmail.com>
2020-05-16 12:16:50 -0400

    linking search btn to results page and determining value of radio btns

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,41 +1,49 @@
 // ------------------ Page 1 ---------------------------
-//stuff Jeff gave us
-      /* var queryString = window.location.search;
-      var urlParams = new URLSearchParams(queryString);
-      var keyword = urlParams.get("keyword");
-      //var sumbitBtn = $("#submit-btn");
-
-      } */
-      
-//=============Radio button variables========================
-
-
-var sumbitBtn = document.getElementById("submit-btn");
-
-function submitButton (){
-  var outdoorRadio = document.querySelector("#outdoor-areas");
-  var restaurantsRadio = document.querySelector("#restaurants");
-  var popularRadio = document.querySelector("#popular");
-  var eventsRadio = document.querySelector("#events");
-
-  if(outdoorRadio.checked == true || restaurantsRadio.checked == true || popularRadio.checked == true || eventsRadio.checked == true){
-    alert("options are selected")
-    loadResultsPage ();
-  }else{
-    alert("Please choose a Search Criteria.")
-  }
+var urlArray = ["outdoor-areas", "restaurants", "popular", "events"];
+//takes a word, checks if its true returns true or false
+function checkIfParamIsTrue(word) {
+  var queryString = window.location.search;
+  console.log(queryString);
+  var urlParams = new URLSearchParams(queryString);
+  console.log(urlParams);
+  var keyword = urlParams.get(word);
+  return keyword === "true";
   
 }
 
-function loadResultsPage(){
 
+//=============Submit button========================
 
+function submitButton() {
+  /* var outdoorRadio = document.querySelector("#outdoor-areas");
+  var restaurantsRadio = document.querySelector("#restaurants");
+  var popularRadio = document.querySelector("#popular");
+  var eventsRadio = document.querySelector("#events"); */
+
+  var urlParams = "?";
+
+  for (i = 0; i < urlArray.length; i++) {
+    var checkBox = document.querySelector("#" + urlArray[i]);
+    if (checkBox.checked == true) {
+      //adding string that got the id 
+      urlParams += urlArray[i] + "=true&";
+    }
+  }
+  console.log(urlParams);
+  //combining route of file with params
+  window.location = "./assets/results.html" + urlParams;
+  console.log(location.href);
+ 
 }
 
-
-
-
-
+var submitBtn = document.getElementById("submit-btn");
+if (submitBtn) {
+  sumbitBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    submitButton();
+    
+  });
+}
 
 // // ------------------- Page 2 ------------------------------
 var map;
@@ -49,3 +57,19 @@ function initMap() {
 }
 
 
+//selects card container on results page and determines which radio button was selected and should be displayed.
+var cardContainer = document.getElementById("card-container");
+if (cardContainer) {
+  for (i = 0; i < urlArray.length; i++) {
+    var param = urlArray[i]
+    var paramIsTrue = checkIfParamIsTrue(param);
+    if(paramIsTrue){
+      var cardWithClass = document.getElementsByClassName(param)
+      cardWithClass[0].style.display = "none";
+    }
+    
+    console.log(param, paramIsTrue)
+}
+
+  
+}
